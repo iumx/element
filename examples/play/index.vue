a9f71c3235682f9b817a69a04cbc3a5c11fd4962
Aex <spryti@qq.com>
2021-08-23 16:43:35 +0000

    Add Play

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div style="margin: 20px;">
-    <el-input v-model="input" placeholder="请输入内容"></el-input>
+          <el-time-picker style="margin-left: 20px;"
+    v-model="value1"
+    format="HH:mm:ss"
+    placeholder="任意时间点">
+  </el-time-picker>
+
+  <el-time-picker style="margin-left: 20px;"
+    v-model="value1"
+    format="时间：HH时mm分ss秒"
+    placeholder="任意时间点">
+  </el-time-picker>
+
+    <el-time-picker style="margin-left: 20px;"
+    v-model="value1"
+    format="前缀 HH:mm:ss 后缀"
+    placeholder="任意时间点">
+  </el-time-picker>
+
+
+
   </div>
 </template>
 
@@ -8,7 +27,7 @@
   export default {
     data() {
       return {
-        input: 'Hello Element UI!'
+         value1: new Date()
       };
     }
   };
